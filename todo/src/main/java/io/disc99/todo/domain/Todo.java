cea1c1905c1ba5971e424ac21b262eed40a7ed07
d.kanehira <disc99.g@gmail.com>
2016-06-27 22:18:10 +0900

    Add todo properties

--- todo/src/main/java/io/disc99/todo/domain/Todo.java
+++ todo/src/main/java/io/disc99/todo/domain/Todo.java
@@ -2,9 +2,14 @@ package io.disc99.todo.domain;
 
 import io.disc99.archetype.Entity;
 import io.disc99.archetype.Identify;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class Todo implements Entity {
+
     Identify identify;
+    Doing doing;
+    DoneAt doneAt;
 
     public void modifed() {
         // TODO
