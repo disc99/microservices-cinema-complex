1a5bb36a4b862bd750351759df9ec5dbbaa78bdc
d.kanehira <disc99.g@gmail.com>
2016-07-02 11:32:10 +0900

    Add ModifyHandler

---
+++ todo/src/main/java/io/disc99/todo/application/ModifyHandler.java
@@ -0,0 +1,10 @@
+package io.disc99.todo.application;
+
+import io.disc99.archetype.CommandHandler;
+
+public class ModifyHandler implements CommandHandler<Modify> {
+    @Override
+    public void handle(Modify command) {
+
+    }
+}
