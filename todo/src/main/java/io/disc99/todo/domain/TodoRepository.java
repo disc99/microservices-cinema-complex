86a09c55aedc7bb4cccde22127469364f47ffe21
d.kanehira <disc99.g@gmail.com>
2016-07-03 11:32:10 +0900

    Add find interface with TodoRepository

--- todo/src/main/java/io/disc99/todo/domain/TodoRepository.java
+++ todo/src/main/java/io/disc99/todo/domain/TodoRepository.java
@@ -1,6 +1,8 @@
 package io.disc99.todo.domain;
 
+import io.disc99.archetype.Identify;
 import io.disc99.archetype.Repository;
 
 public interface TodoRepository extends Repository {
+    Todo find(Identify identify);
 }
--- todo/src/main/java/io/disc99/todo/infrastructure/TodoRepositoryDb.java
+++ todo/src/main/java/io/disc99/todo/infrastructure/TodoRepositoryDb.java
@@ -1,6 +1,13 @@
 package io.disc99.todo.infrastructure;
 
+import io.disc99.archetype.Identify;
+import io.disc99.todo.domain.Todo;
 import io.disc99.todo.domain.TodoRepository;
 
 public class TodoRepositoryDb implements TodoRepository {
+    @Override
+    public Todo find(Identify identify) {
+        // TODO
+        return null;
+    }
 }
