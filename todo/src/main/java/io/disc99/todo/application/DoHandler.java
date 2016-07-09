7fc8688a0ab5eb1f66de2dbea3a0660eca336581
d.kanehira <disc99.g@gmail.com>
2016-07-09 11:15:53 +0900

    Add DoHandler

---
+++ todo/src/main/java/io/disc99/todo/application/DoHandler.java
@@ -0,0 +1,10 @@
+package io.disc99.todo.application;
+
+import io.disc99.archetype.CommandHandler;
+
+public class DoHandler implements CommandHandler<Do> {
+    @Override
+    public void handle(Do command) {
+
+    }
+}
