2bcae4bfc9d53a4fb4aa55265b53e5b7a1ff28c0
d.kanehira <disc99.g@gmail.com>
2016-07-13 21:15:30 +0900

    Add "Add" domain logic

--- todo/src/main/java/io/disc99/todo/application/AddHandler.java
+++ todo/src/main/java/io/disc99/todo/application/AddHandler.java
@@ -1,6 +1,8 @@
 package io.disc99.todo.application;
 
 import io.disc99.archetype.CommandHandler;
+import io.disc99.todo.domain.Added;
+import io.disc99.todo.domain.TodoList;
 import io.disc99.todo.domain.TodoRepository;
 import lombok.AllArgsConstructor;
 
@@ -11,5 +13,6 @@ public class AddHandler implements CommandHandler<Add> {
 
     @Override
     public void handle(Add add) {
+        new TodoList().add(new Added(add.identify(), add.doing()));
     }
 }
--- todo/src/main/java/io/disc99/todo/domain/TodoList.java
+++ todo/src/main/java/io/disc99/todo/domain/TodoList.java
@@ -1,18 +1,22 @@
 package io.disc99.todo.domain;
 
+import io.disc99.ObjectProvider;
+import io.disc99.archetype.EventBus;
 import io.disc99.archetype.ValueObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TodoList implements ValueObject {
+
+    private final EventBus eventBus = ObjectProvider.provide(EventBus.class);
     List<Todo> list;
 
     public TodoList() {
         list = new ArrayList<>();
     }
 
-    public void add() {
-        // TODO
+    public void add(Added added) {
+        eventBus.publish(added);
     }
 }
