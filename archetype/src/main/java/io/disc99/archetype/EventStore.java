8ce1339c9aa0560db78cec5fd48a613c6af74e91
d.kanehira <disc99.g@gmail.com>
2016-07-21 22:07:17 +0900

    Add Event store

---
+++ archetype/src/main/java/io/disc99/archetype/EventStore.java
@@ -0,0 +1,11 @@
+package io.disc99.archetype;
+
+public class EventStore {
+
+    public void add(DomainEvent event) {
+
+    }
+
+    public EventStream stream(Class<DomainEvent> clazz, Identify identify) {
+    }
+}
