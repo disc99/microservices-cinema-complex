38f8f459f8e8258eaedd172984c191c32f1b0a3e
d.kanehira <disc99.g@gmail.com>
2016-08-02 22:41:27 +0900

    Add domain event constructor

--- todo/src/main/java/io/disc99/todo/domain/Todo.java
+++ todo/src/main/java/io/disc99/todo/domain/Todo.java
@@ -1,10 +1,10 @@
 package io.disc99.todo.domain;
 
-import io.disc99.archetype.Entity;
-import io.disc99.archetype.EventBus;
-import io.disc99.archetype.Identify;
+import io.disc99.archetype.*;
 import io.disc99.ObjectProvider;
 import lombok.AllArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 public class Todo implements Entity {
@@ -14,6 +14,10 @@ public class Todo implements Entity {
     Identify identify;
     Doing doing;
     DoneAt doneAt;
+
+    public Todo(List<DomainEvent> domainEvents) {
+        // TODO
+    }
 
     public void modified(Doing doing) {
         Modified modified = new Modified(identify, doing);
