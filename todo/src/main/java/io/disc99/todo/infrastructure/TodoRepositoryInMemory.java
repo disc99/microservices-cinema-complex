f0e9ce36b907a7fef6d8edd0c827d2933975b0f3
d.kanehira <disc99.g@gmail.com>
2016-08-03 23:13:41 +0900

    Change TodoRepository impl datasource

--- todo/src/main/java/io/disc99/todo/infrastructure/TodoRepositoryInMemory.java
+++ todo/src/main/java/io/disc99/todo/infrastructure/TodoRepositoryInMemory.java
@@ -4,7 +4,8 @@ import io.disc99.archetype.Identify;
 import io.disc99.todo.domain.Todo;
 import io.disc99.todo.domain.TodoRepository;
 
-public class TodoRepositoryDb implements TodoRepository {
+public class TodoRepositoryInMemory implements TodoRepository {
+
     @Override
     public Todo find(Identify identify) {
         // TODO
