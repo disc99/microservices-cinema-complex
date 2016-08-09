b7e9ac6d4d8a22e962800fae5275237f131e36a5
d.kanehira <disc99.g@gmail.com>
2016-08-09 20:41:38 +0900

    Add repository conig

--- todo/src/main/java/io/disc99/TodoConfig.java
+++ todo/src/main/java/io/disc99/TodoConfig.java
@@ -1,7 +1,17 @@
 package io.disc99;
 
+import io.disc99.archetype.CommandBus;
+import io.disc99.todo.application.ModifyHandler;
+import io.disc99.todo.domain.TodoRepository;
+import io.disc99.todo.infrastructure.TodoRepositoryInMemory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TodoConfig {
+
+    @Bean
+    TodoRepository todoRepository() {
+        return new TodoRepositoryInMemory();
+    }
 }
