d4cc1cd739e55a7cda3d33b97251d14418e8b655
d.kanehira <disc99.g@gmail.com>
2016-08-14 10:19:41 +0900

    Add added event handler

---
+++ todo/src/main/java/io/disc99/todo/domain/AddedHandler.java
@@ -0,0 +1,10 @@
+package io.disc99.todo.domain;
+
+import io.disc99.archetype.EventHandler;
+
+public class AddedHandler implements EventHandler<Added> {
+    @Override
+    public void on(Added event) {
+
+    }
+}
