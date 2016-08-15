519026772dd73602dd18fba9f405b653c8ef93c1
d.kanehira <disc99.g@gmail.com>
2016-08-15 23:11:18 +0900

    Add added event handler logic

--- todo/src/main/java/io/disc99/todo/domain/Added.java
+++ todo/src/main/java/io/disc99/todo/domain/Added.java
@@ -3,8 +3,12 @@ package io.disc99.todo.domain;
 import io.disc99.archetype.DomainEvent;
 import io.disc99.archetype.Identify;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 
 @AllArgsConstructor
+@Getter
+@Accessors(fluent = true)
 public class Added implements DomainEvent {
     private TodoId todoId;
     private Doing doing;
--- todo/src/main/java/io/disc99/todo/domain/AddedHandler.java
+++ todo/src/main/java/io/disc99/todo/domain/AddedHandler.java
@@ -1,10 +1,17 @@
 package io.disc99.todo.domain;
 
 import io.disc99.archetype.EventHandler;
+import io.disc99.archetype.EventStore;
+import io.disc99.archetype.EventStreamId;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class AddedHandler implements EventHandler<Added> {
+
+    private EventStore eventStore;
+
     @Override
     public void on(Added event) {
-
+        eventStore.add(new EventStreamId(Added.class, event.todoId()), event);
     }
 }
