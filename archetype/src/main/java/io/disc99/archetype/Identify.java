49f029a89201c6ef26a9b5bf2a6f663c5c366268
d.kanehira <disc99.g@gmail.com>
2016-08-16 22:35:17 +0900

    Change Identify from class to interface

--- archetype/src/main/java/io/disc99/archetype/Identify.java
+++ archetype/src/main/java/io/disc99/archetype/Identify.java
@@ -1,17 +1,4 @@
 package io.disc99.archetype;
 
 
-import lombok.EqualsAndHashCode;
-
-import java.util.UUID;
-
-@EqualsAndHashCode
-public class Identify {
-    String value;
-    public Identify() {
-        this.value = UUID.randomUUID().toString();
-    }
-    public Identify(String value) {
-        this.value = value;
-    }
-}
+public interface Identify {}
