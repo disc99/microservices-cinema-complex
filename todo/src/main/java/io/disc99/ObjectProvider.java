bf3bedcbc431ff8f6bb6a83d764b3fc0eda46d74
d.kanehira <disc99.g@gmail.com>
2016-08-20 12:31:10 +0900

    Fix NPE

--- todo/src/main/java/io/disc99/ObjectProvider.java
+++ todo/src/main/java/io/disc99/ObjectProvider.java
@@ -4,7 +4,9 @@ package io.disc99;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ObjectProvider implements ApplicationContextAware {
 
     private static ApplicationContext context;
