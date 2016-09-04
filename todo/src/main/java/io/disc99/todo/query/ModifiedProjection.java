ce91f9c19f2393e03a6bf29662f2c1a3a8ddc7d7
d.kanehira <disc99.g@gmail.com>
2016-09-04 20:11:01 +0900

    Add modified projection

---
+++ todo/src/main/java/io/disc99/todo/query/ModifiedProjection.java
@@ -0,0 +1,11 @@
+package io.disc99.todo.query;
+
+import io.disc99.archetype.EventHandler;
+import io.disc99.todo.domain.Modified;
+
+public class ModifiedProjection implements EventHandler<Modified> {
+    @Override
+    public void on(Modified event) {
+
+    }
+}
