fa42f10f89cef24e6736de22111316b6974c8521
d.kanehira <disc99.g@gmail.com>
2016-09-05 22:43:10 +0900

    Add done projection

---
+++ todo/src/main/java/io/disc99/todo/query/DoneProjection.java
@@ -0,0 +1,11 @@
+package io.disc99.todo.query;
+
+import io.disc99.archetype.EventHandler;
+import io.disc99.todo.domain.Done;
+
+public class DoneProjection implements EventHandler<Done> {
+    @Override
+    public void on(Done event) {
+
+    }
+}
