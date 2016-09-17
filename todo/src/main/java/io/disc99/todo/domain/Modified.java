d77515446447f20bf2386ba6a97833fb1c85309b
d.kanehira <disc99.g@gmail.com>
2016-09-17 19:14:35 +0900

    Add modified projection logic

--- todo/src/main/java/io/disc99/todo/domain/Modified.java
+++ todo/src/main/java/io/disc99/todo/domain/Modified.java
@@ -3,9 +3,13 @@ package io.disc99.todo.domain;
 import io.disc99.archetype.DomainEvent;
 import io.disc99.archetype.Identify;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 
 @AllArgsConstructor
+@Getter
+@Accessors(fluent = true)
 public class Modified implements DomainEvent {
-    private Identify identify;
+    private TodoId todoId;
     private Doing doing;
 }
--- todo/src/main/java/io/disc99/todo/query/ModifiedProjection.java
+++ todo/src/main/java/io/disc99/todo/query/ModifiedProjection.java
@@ -3,16 +3,20 @@ package io.disc99.todo.query;
 import io.disc99.archetype.EventHandler;
 import io.disc99.todo.domain.Modified;
 import lombok.AllArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @AllArgsConstructor
 public class ModifiedProjection implements EventHandler<Modified> {
 
-    NamedParameterJdbcTemplate  namedParameterJdbcTemplate;
+    NamedParameterJdbcTemplate  jdbcTemplate;
 
     @Override
     public void on(Modified event) {
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("ID", event.todoId().value())
+                .addValue("DOING", event.doing().value());
+        jdbcTemplate.update("INSERT INTO TODOS(ID, DOING) VALUES(:id, :doing)", params);
 
     }
 }
