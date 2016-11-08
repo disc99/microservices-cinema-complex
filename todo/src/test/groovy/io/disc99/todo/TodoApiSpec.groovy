b28717f443bfc708b61a8233a4e6320136dab412
d.kanehira <disc99.g@gmail.com>
2016-11-08 22:41:39 +0900

    Create todo api sprc

---
+++ todo/src/test/groovy/io/disc99/todo/TodoApiSpec.groovy
@@ -0,0 +1,20 @@
+package io.disc99.todo
+
+import spock.lang.Specification
+
+
+class TodoApiSpec extends Specification {
+
+    def "todo life cycle"() {
+
+    }
+
+    def "todo validation"() {
+
+    }
+
+    def "todo error"() {
+
+    }
+
+}
