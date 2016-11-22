491696e1bb85976d4f3f706b9ea30e5088f39792
d.kanehira <disc99.g@gmail.com>
2016-11-22 19:18:01 +0900

    Create UI controller

---
+++ ui/src/main/java/io/disc99/ui/UiController.java
@@ -0,0 +1,13 @@
+package io.disc99.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UiController {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World";
+    }
+
+}
