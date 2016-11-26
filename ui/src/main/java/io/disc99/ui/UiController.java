ad911c540512c9abb87aed77fff834b3a3ad9609
d.kanehira <disc99.g@gmail.com>
2016-11-27 00:51:26 +0900

    Change UI for display name

--- ui/src/main/java/io/disc99/ui/UiController.java
+++ ui/src/main/java/io/disc99/ui/UiController.java
@@ -2,14 +2,19 @@ package io.disc99.ui;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @EnableOAuth2Sso
 public class UiController {
 
     @RequestMapping("/")
-    String home() {
+    String home(Principal user, Model model) {
+        model.addAttribute("name", user.getName());
         return "index";
     }
+
 }
