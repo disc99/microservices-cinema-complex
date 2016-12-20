6dfe9c5c6632210ecdd892797cc461be7d7ca13f
d.kanehira <disc99.g@gmail.com>
2016-12-21 00:19:52 +0900

    Add service

---
+++ screen/src/main/java/io/disc99/cinemapalace/screen/Screen.java
@@ -0,0 +1,10 @@
+package io.disc99.cinemapalace.screen;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Screen {
+    @Id
+    @GeneratedValue
+    Long no;
+}
---
+++ screen/src/main/java/io/disc99/cinemapalace/screen/ScreenrRepository.java
@@ -0,0 +1,6 @@
+package io.disc99.cinemapalace.screen;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScreenrRepository extends JpaRepository<Screen, Long> {
+}
